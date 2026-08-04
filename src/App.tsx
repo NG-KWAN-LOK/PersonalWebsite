1966a4ec0435f0476a11811832d2bca024666edf
kwanlok.ng <kwanlok.ng@junyiacademy.org>
2026-08-04 23:01:01 +0800

    new(routing): add route structure with catch-all redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Redirect, Route, Switch } from "react-router-dom";
 import { HomePage } from "./components/home-page/HomePage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Switch>
+      {/* Add other pages here, e.g. <Route path='/other-page/foo'><Foo /></Route> */}
+      <Route exact path='/'>
+        <HomePage />
+      </Route>
+      <Route path='*'>
+        <Redirect to='/' />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
